646bf7529dbb6d22f359c05cbfcb0b1ecd2a81f1
Daniel Ecer <de-code@users.noreply.github.com>
2025-06-13 18:46:28 +0100

    Added support for response template

--- py_conf_mcp/tools/sources/web_api.py
+++ py_conf_mcp/tools/sources/web_api.py
@@ -56,13 +56,14 @@ def get_requests_auth(
     )
 
 
-class WebApiTool(ToolClass):
+class WebApiTool(ToolClass):  # pylint: disable=too-many-instance-attributes
     def __init__(  # pylint: disable=too-many-arguments
         self,
         url: str,
         *,
         query_parameters: Optional[Mapping[str, str]] = None,
         json_template: Optional[str] = None,
+        response_template: Optional[str] = None,
         headers: Optional[Mapping[str, str]] = None,
         method: str = 'GET',
         verify_ssl: bool = True,
@@ -72,6 +73,7 @@ class WebApiTool(ToolClass):
         self.url = url
         self.query_parameters = query_parameters or {}
         self.json_template = json_template
+        self.response_template = response_template
         self.method = method
         self.verify_ssl = verify_ssl
         self.headers = headers
@@ -104,5 +106,15 @@ class WebApiTool(ToolClass):
         )
         response.raise_for_status()
         response_json = response.json()
-        LOGGER.info('response_json: %r', response_json)
-        return response_json
+        if not self.response_template:
+            LOGGER.info('response_json: %r', response_json)
+            return response_json
+        response_content = get_evaluated_template(
+            self.response_template,
+            variables={
+                'response_json': response_json,
+                'params': params
+            }
+        )
+        LOGGER.info('response_content after template: %r', response_content)
+        return response_content
