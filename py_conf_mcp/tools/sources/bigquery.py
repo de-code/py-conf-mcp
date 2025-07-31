7f91cc02627bad6ecdda6d5da86e0918a38dbf95
Daniel Ecer <de-code@users.noreply.github.com>
2025-07-31 13:23:54 +0100

    Implemented BigQueryTool with fixed SQL query

---
+++ py_conf_mcp/tools/sources/bigquery.py
@@ -0,0 +1,67 @@
+import logging
+from typing import Any, Iterable, Sequence
+
+from google.cloud import bigquery
+from google.cloud.bigquery.table import RowIterator
+
+from py_conf_mcp.tools.typing import ToolClass
+from py_conf_mcp.utils.json import get_json_as_csv_lines
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def get_bq_client(project_name: str) -> bigquery.Client:
+    return bigquery.Client(project=project_name)
+
+
+def get_bq_result_from_bq_query(
+    project_name: str,
+    query: str,
+    query_parameters: Sequence[Any] | None = tuple()
+) -> RowIterator:
+    client = get_bq_client(project_name=project_name)
+    job_config = bigquery.QueryJobConfig(query_parameters=query_parameters)
+    query_job = client.query(query, job_config=job_config)  # Make an API request.
+    bq_result = query_job.result()  # Waits for query to finish
+    LOGGER.debug('bq_result: %r', bq_result)
+    return bq_result
+
+
+def iter_dict_from_bq_query(
+    project_name: str,
+    query: str,
+    query_parameters: Sequence[Any] | None = tuple()
+) -> Iterable[dict]:
+    bq_result = get_bq_result_from_bq_query(
+        project_name=project_name,
+        query=query,
+        query_parameters=query_parameters
+    )
+    for row in bq_result:
+        LOGGER.debug('row: %r', row)
+        yield dict(row.items())
+
+
+class BigQueryTool(ToolClass):  # pylint: disable=too-many-instance-attributes
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        *,
+        project_name: str,
+        sql_query: str,
+        output_format: str = 'json'
+    ):
+        super().__init__()
+        self.project_name = project_name
+        self.sql_query = sql_query
+        self.output_format = output_format
+
+    def __call__(self, **kwargs):
+        result: Any = list(iter_dict_from_bq_query(
+            project_name=self.project_name,
+            query=self.sql_query
+        ))
+        if self.output_format == 'csv':
+            result = '\n'.join(get_json_as_csv_lines(result))
+        LOGGER.info('query results: %r', result)
+        return result
